37f766d74c74e478d3fbc588a4ef971d9b7d378f
Alan Hecht <aghecht42@gmail.com>
2012-06-02 18:37:16 -0400

    [AH] Set up gmail account

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,11 +24,11 @@ IcandothatSignmeup::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "icandothat.org",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: "rhokicandothat",
+    password: "this15rails"
   }
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,8 @@ IcandothatSignmeup::Application.configure do
     domain: "example.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: "rhokicandothat",
+    password: "this15rails"
   }
 
 
