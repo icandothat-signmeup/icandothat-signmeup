e5379ee0ed3ee1f79667a968887840da494a073f
Alan Hecht <aghecht42@gmail.com>
2012-06-02 17:09:59 -0400

    [AH] Display events on calendar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,6 +8,7 @@ class EventsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @events }
+      format.js
     end
   end
 
