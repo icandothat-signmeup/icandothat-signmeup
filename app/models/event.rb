9fc1550ad7cfb1af122f606bf1560c4003c97046
Alan Hecht <aghecht42@gmail.com>
2012-06-02 16:38:29 -0400

    [AH] Created commitments along with Event/User relationship

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
+  has_many :commitments
+  has_many :users, :through => :commitments
+
   attr_accessible :commitment_type, :description, :end_time, :location, :max_volunteers, :min_volunteers, :start_time, :title
 end
